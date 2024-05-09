a3ea6584e0ae0e3196bd8b121881c7315d791765
Anas Chami <anaschami@laposte.net>
2024-05-09 20:44:43 +0200

    ajout des fichiers clock.py, main.py, stopwatch.py, et ui.py. Ajout des premières lignes dans clock.py (pour l'horloge) pour importer les premiers modules

---
+++ clock.py
@@ -0,0 +1,5 @@
+# Importe la bibliothèque tkinter pour la création d'interfaces graphiques et l'aliaser sous le nom de 'tk' pour plus de commodité
+import tkinter as tk
+
+# Importe la fonction strftime du module time pour formater les chaînes de temps
+from time import strftime
